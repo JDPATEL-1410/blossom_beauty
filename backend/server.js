faff2e496860ad068f9b34ee60aebbe06dca3bcc
Ishwar Prajapati <prajapatiishwar79@gmail.com>
2026-06-25 23:36:55 +0530

    Update cors middleware

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,11 @@ const app = express();
 // Connect to database
 connectDB();
 
-// Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 
 // Routes
